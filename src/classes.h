a2c391b608d80f0d8ee928d0e4ed8bc415fadbc7
cmsbuild <>
2007-08-07 15:18:11 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- GsfTrackReco-CMSSW_1_7_0_pre3'.
    Sprout from master 2007-01-26 10:39:04 UTC Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch> 'Moved Gsf* definitions from TrackReco to GsfTrackReco.'
    Cherrypick from master 2007-08-07 15:18:03 UTC Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch> 'changes suggested by A. Bocci for access using RefToBase<Track>':
        src/classes.h
        src/classes_def.xml

--- src/classes.h
+++ src/classes.h
@@ -1,8 +1,17 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "Rtypes.h" 
+#include "Math/Cartesian3D.h" 
+#include "Math/Polar3D.h" 
+#include "Math/CylindricalEta3D.h" 
+#include <boost/cstdint.hpp> 
+#include "DataFormats/TrackReco/interface/TrackFwd.h" 
 #include "DataFormats/GsfTrackReco/interface/GsfTrackFwd.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrackExtra.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrackExtraFwd.h" 
 #include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/Common/interface/RefHolder.h"
+#include "DataFormats/Common/interface/Holder.h"
 #include <vector>
 
 namespace {
@@ -21,5 +30,9 @@ namespace {
 
     edm::helpers::Key< edm::RefProd < std::vector < reco::GsfTrack > > > rpt11;
     edm::AssociationMap<edm::OneToValue< std::vector<reco::GsfTrack>, double, unsigned int > > am11;
+
+    // RefToBase<reco::Track>
+    edm::reftobase::Holder<reco::Track, reco::GsfTrackRef>  h_tk_gtkr;
+    edm::reftobase::RefHolder<reco::GsfTrackRef>            rf_gtkr;
   }
 }
